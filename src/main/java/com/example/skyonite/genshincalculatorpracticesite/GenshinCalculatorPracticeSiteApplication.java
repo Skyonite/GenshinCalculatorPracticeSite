70c7ac50bf79c92f992e9bcab798322e5524a52b
Skyonite <skyonite@gmail.com>
2022-03-15 05:14:28 +0100

    Created project, is this how git works

---
+++ src/main/java/com/example/skyonite/genshincalculatorpracticesite/GenshinCalculatorPracticeSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyonite.genshincalculatorpracticesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinCalculatorPracticeSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinCalculatorPracticeSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skyonite/genshincalculatorpracticesite/GenshinCalculatorPracticeSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyonite.genshincalculatorpracticesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinCalculatorPracticeSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
